335e95a94ad3f0f1b218fbac9186af4534deb295
Kayz <kayz_lhy@outlook.com>
2024-07-09 22:39:37 +0800

    init

---
+++ gpqh-main/src/main/java/com/gpqh/main/GpqhMainApplication.java
@@ -0,0 +1,13 @@
+package com.gpqh.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpqhMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpqhMainApplication.class, args);
+    }
+
+}
---
+++ gpqh-main/src/test/java/com/gpqh/main/GpqhMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpqh.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpqhMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
