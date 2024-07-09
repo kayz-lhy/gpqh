99a9520f2e3cdcbd38d54d6e28ac31bc3bd04e27
Kayz <kayz_lhy@outlook.com>
2024-07-09 22:32:35 +0800

    init

---
+++ gpqh-user/src/test/java/com/gpqh/user/GpqhUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpqh.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpqhUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
